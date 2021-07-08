862835c2ad65f708de1831f67ba618f2babd1771
maleknej <maleknej@ualberta.ca>
2021-07-08 16:54:27 -0600

    Setup app fot Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-// const { response } = require("express")
-
-console.log('Client side Js')
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1') 
@@ -22,7 +11,7 @@ weatherForm.addEventListener('submit',(event)=>{
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = '' 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent = data.error
